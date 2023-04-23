ff3447e00743f9b377e6b8529053cb615058346e
dacc93 <dacc.93@gmail.com>
2023-04-23 11:33:08 -0500

    columnas calificaciones a validar

--- leer_archivos.py
+++ leer_archivos.py
@@ -2,10 +2,12 @@ import pandas as pd
 import os
 
 # Definir las columnas a incluir en el dataframe final
-columnas = ['programa', 'año', 'Colegio', 'Sede', 'Grado', 'Grupo' ]
-archivo = 'BBDD_Ortigal_Salida_2021.xlsx'
-df = pd.read_excel(os.path.join('Base', archivo), usecols=columnas, sheet_name='2°')
-print(df)
+# columnas = ['programa', 'año', 'colegio', 'sede', 'grado', 'grupo', 'jornada', 'departamento', 'municipio', 'género'  ]
+columnas = ['lectura_correctas', 'comprension_correctas', 'oral_correctas', 'comparacion_correctas', 
+            'numero_faltante_correctas', 'sumas_correctas', 'restas_correctas' ]
+# archivo = 'BBDD_Ortigal_Salida_2021.xlsx'
+# df = pd.read_excel(os.path.join('Base', archivo), usecols=columnas, sheet_name='2°')
+
 print('lectura')
 # Inicializar una lista para guardar los dataframes de cada archivo
 df_list = []
@@ -17,8 +19,9 @@ for archivo in os.listdir('Base'):
     if archivo.endswith('.xlsx'):
         print('nombre archivo analizado',archivo)
         # Leer el archivo y guardar únicamente las columnas necesarias
-        df = pd.read_excel(os.path.join('Base', archivo), usecols=columnas, sheet_name='2°')
-
+        df = pd.read_excel(os.path.join('Base', archivo), sheet_name='2°')
+        df.columns = df.columns.str.lower()
+        df = df.loc[:, columnas]
         # Agregar el nombre del archivo como una columna en el dataframe
         df['archivo'] = archivo
 
