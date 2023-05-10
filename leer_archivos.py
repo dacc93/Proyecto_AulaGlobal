b7472283653e25e084a04078a6df663d80735b42
Isco8809 <francisco.echeverri@bambooanalytics.com.co>
2023-05-09 22:26:46 -0500

    Verificacion

--- leer_archivos.py
+++ leer_archivos.py
@@ -38,31 +38,44 @@
 import pandas as pd
 import os
 
-# Lista con los nombres de las columnas a buscar
-columnas_buscar = ["nombre_columna1", "nombre_columna2", "nombre_columna3"]
-# columnas = ['lectura_correctas', 'comprension_correctas', 'oral_correctas', 'comparacion_correctas', 
-#             'numero_faltante_correctas', 'sumas_correctas', 'restas_correctas' ]
-columnas =['programa','año', 'Departamento', 'Municipio', 'Colegio', 'Sede', 'tratamiento/control', 'focalizado', 
-           'Género', 'Grado', 'Grupo', 'Jornada', 'ID']
+# # Lista con los nombres de las columnas a buscar
+# columnas_buscar = ["nombre_columna1", "nombre_columna2", "nombre_columna3"]
+# # columnas = ['lectura_correctas', 'comprension_correctas', 'oral_correctas', 'comparacion_correctas', 
+# #             'numero_faltante_correctas', 'sumas_correctas', 'restas_correctas' ]
+# columnas =['programa','año', 'Departamento', 'Municipio', 'Colegio', 'Sede', 'tratamiento/control', 'focalizado', 
+#            'Género', 'Grado', 'Grupo', 'Jornada', 'ID']
 
-archivos_en_carpeta = os.listdir('Base')
+# archivos_en_carpeta = os.listdir('Base')
 
-# Abrir archivo de texto para guardar los nombres de las columnas que no se encuentren
-with open("columnas_faltantes3.txt", "w") as archivo_txt:
+# # Abrir archivo de texto para guardar los nombres de las columnas que no se encuentren
+# with open("columnas_faltantes3.txt", "w") as archivo_txt:
     
-    archivos_excel = [archivo for archivo in archivos_en_carpeta if archivo.endswith(".xlsx")]
-    # print(archivos_excel)
-    # Ciclo para leer cada archivo excel
-    for nombre_archivo in archivos_excel:
+#     archivos_excel = [archivo for archivo in archivos_en_carpeta if archivo.endswith(".xlsx")]
+#     # print(archivos_excel)
+#     # Ciclo para leer cada archivo excel
+#     for nombre_archivo in archivos_excel:
         
-        # Leer archivo excel con pandas
-        df = pd.read_excel(os.path.join('Base', nombre_archivo), sheet_name='3°')
-        print(nombre_archivo) 
-        # Verificar si las columnas buscadas existen en el archivo
-        columnas_existentes = df.columns.tolist()
-        columnas_faltantes = set(columnas) - set(columnas_existentes)
+#         # Leer archivo excel con pandas
+#         df = pd.read_excel(os.path.join('Base', nombre_archivo), sheet_name='3°')
+#         print(nombre_archivo) 
+#         # Verificar si las columnas buscadas existen en el archivo
+#         columnas_existentes = df.columns.tolist()
+#         columnas_faltantes = set(columnas) - set(columnas_existentes)
         
-        # Si faltan columnas, guardar sus nombres en el archivo de texto
-        if columnas_faltantes:
-            mensaje = f"Archivo {nombre_archivo}: faltan columnas {columnas_faltantes}\n"
-            archivo_txt.write(mensaje)
+#         # Si faltan columnas, guardar sus nombres en el archivo de texto
+#         if columnas_faltantes:
+#             mensaje = f"Archivo {nombre_archivo}: faltan columnas {columnas_faltantes}\n"
+#             archivo_txt.write(mensaje)
+
+
+df1 = pd.read_excel('C:/Users/franc/OneDrive - Universidad Icesi (@icesi.edu.co)/Escritorio/Prueba trabajo/Trabajo_AulaGlobal/Proyecto_AulaGlobal/Base organizada/BBDD_AG_JARPMJ_2022/BBDD_AG_JARPMJ_Entrada_2022.xlsx', sheet_name='2°', usecols=['ID_cruce'])
+df2 = pd.read_excel('C:/Users/franc/OneDrive - Universidad Icesi (@icesi.edu.co)/Escritorio/Prueba trabajo/Trabajo_AulaGlobal/Proyecto_AulaGlobal/Base organizada/BBDD_AG_JARPMJ_2022/BBDD_AG_JARPMJ_Salida_2022.xlsx', sheet_name='2°', usecols=['ID estudiante'])
+df2= df2.rename(columns={'ID estudiante':'ID_cruce'})
+comparacion = df1['ID_cruce'] == df2['ID estudiante']
+
+if df1['ID_cruce'].equals(df2['ID estudiante']):
+    print("Las columnas son iguales")
+else:
+    print("Las columnas son diferentes")
+# resultado = comparacion.value_counts()
+# print(resultado)
